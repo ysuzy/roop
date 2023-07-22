b69f94a5d0269285e9f5d01c4d1f5d015ae634d7
ysuzy <57137978+ysuzy@users.noreply.github.com>
2023-07-22 12:06:59 +0800

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,6 +1,6 @@
 import threading
 import numpy
-import opennsfw2
+#import opennsfw2
 from PIL import Image
 from keras import Model
 
@@ -16,7 +16,7 @@ def get_predictor() -> Model:
 
     with THREAD_LOCK:
         if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
+            # PREDICTOR = opennsfw2.make_open_nsfw_model()
     return PREDICTOR
 
 
@@ -28,16 +28,16 @@ def clear_predictor() -> None:
 
 def predict_frame(target_frame: Frame) -> bool:
     image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
+    #image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     views = numpy.expand_dims(image, axis=0)
     _, probability = get_predictor().predict(views)[0]
     return probability > MAX_PROBABILITY
 
 
 def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+   # return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
 
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
+    _, #probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
     return any(probability > MAX_PROBABILITY for probability in probabilities)
